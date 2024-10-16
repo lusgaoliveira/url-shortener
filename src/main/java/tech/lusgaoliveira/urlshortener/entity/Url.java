885cde9e6153004d9c725f2c8c9df37c61d04706
lusgaoliveira <lucassdeoliveira@outlook.com.br>
2024-10-15 21:40:05 -0300

    feat: add url entity

---
+++ src/main/java/tech/lusgaoliveira/urlshortener/entity/Url.java
@@ -0,0 +1,58 @@
+package tech.lusgaoliveira.urlshortener.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@RedisHash("Url")
+public class Url implements Serializable {
+
+    @Id
+    private String id;
+
+    private String fullUrl;
+
+    @TimeToLive
+    private Long expiresAt;
+
+    public Url(){
+    }
+
+    public Url(String id, String fullUrl) {
+        this.id = id;
+        this.fullUrl = fullUrl;
+    }
+
+    public Url(String id, String fullUrl, Long expiresAt) {
+        this.id = id;
+        this.fullUrl = fullUrl;
+        this.expiresAt = expiresAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullUrl() {
+        return fullUrl;
+    }
+
+    public void setFullUrl(String fullUrl) {
+        this.fullUrl = fullUrl;
+    }
+
+    public Long getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Long expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
